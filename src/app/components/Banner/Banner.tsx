840b826f607af615252ee264d11e168b7f373ae4
Sergey Evdokimov <cadsergrf@yandex.ru>
2024-01-16 17:38:58 +0700

    add banner disign

---
+++ src/app/components/Banner/Banner.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Banner.module.css';
+
+const Banner = () => {
+  return <div className={styles.banner}>Banner</div>;
+};
+
+export { Banner };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import { Header } from './components/Header/Header';
+import { Banner } from './components/Banner/Banner';
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <div className={styles.description}></div>
+        <div className={styles.container}>
+          <Banner />
+        </div>
       </main>
     </>
   );
